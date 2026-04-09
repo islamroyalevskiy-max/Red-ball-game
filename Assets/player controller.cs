09d6d231fde5d914560c484101a3f0966f3ded24
islamroyalevskiy-max <islamroya.levskiy@gmail.com>
2026-04-09 13:38:51 +0300

    add

--- Assets/player controller.cs
+++ Assets/player controller.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scripts : MonoBehaviour
+public class playercontroller : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +15,4 @@ public class scripts : MonoBehaviour
     {
         
     }
-    
 }
